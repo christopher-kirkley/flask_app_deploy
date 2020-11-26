86aac710137fbc8d61903a5378b92c89cfb09401
Mako <ckirkley@gmail.com>
2020-11-25 21:07:21 -0800

    Updated message.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "<h1 style='color:blue'>Hello World!</h1>"
+    return "<h1 style='color:blue'>Welcome to the Potato Farmer Website!</h1>"
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
