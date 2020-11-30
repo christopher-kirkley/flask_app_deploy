d037123f30f7586166320369df50df58ee5d9452
Christopher Kirkley <ckirkley@gmail.com>
2020-11-29 17:07:44 -0800

    Add garbage.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "<h1 style='color:blue'>Welcome to the Potato Farmer Website!</h1>"
+    return "<h1 style='color:blue'>Now do something different to this to his thisajdslfkahjsfljkasl;dfjadsf to the Potato Farmer Website!</h1>"
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
